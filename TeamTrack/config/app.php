69a5c37fb03aecb624b3ffb3b1ab0187ec0e4d90
tapuzaman <tapu.zaman101@gmail.com>
2019-07-20 01:56:17 +0600

    Socialite Providers & Aliases Added

--- TeamTrack/config/app.php
+++ TeamTrack/config/app.php
@@ -165,7 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Laravel\Socialite\SocialiteServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -228,6 +228,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
 
     ],
