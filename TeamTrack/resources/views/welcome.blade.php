4782e7ae92355e5cddde452a2eda3a7d78294e5e
smashfu <smashfu@gmail.com>
2019-07-20 13:38:36 +0600

    Welcome page

--- TeamTrack/resources/views/welcome.blade.php
+++ TeamTrack/resources/views/welcome.blade.php
@@ -12,8 +12,8 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
-                color: #636b6f;
+                background-color: #ffffff;
+                color: #000000;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
                 height: 100vh;
