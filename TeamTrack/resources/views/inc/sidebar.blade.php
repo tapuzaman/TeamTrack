0bf087243b914898d4fd634b4934a5b8b4a64db1
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-07-25 23:45:58 +0600

    Refactored applayout header imports

--- TeamTrack/resources/views/inc/sidebar.blade.php
+++ TeamTrack/resources/views/inc/sidebar.blade.php
@@ -24,24 +24,29 @@
           </form>
           <div class="nav-wrapper">
             <ul class="nav flex-column">
+            <!------ SIDEBAR LINKS ---------->
+
               <li class="nav-item">
                 <a class="nav-link" href="/home">
                   <i class="material-icons">edit</i>
                   <span>Dashboard</span>
                 </a>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link " href="/tasks">
                   <i class="material-icons">vertical_split</i>
                   <span>All Tasks</span>
                 </a>
               </li>
+
               <li class="nav-item">
                 <a class="nav-link " href="/tasks/create">
                   <i class="material-icons">note_add</i>
                   <span>Add Task</span>
                 </a>
               </li>
+              
               <li class="nav-item">
                 <a class="nav-link " href="/teams/index">
                   <i class="material-icons">view_module</i>
--- TeamTrack/resources/views/layouts/app.blade.php
+++ TeamTrack/resources/views/layouts/app.blade.php
@@ -1,58 +1,43 @@
 <!DOCTYPE html>
 <html class="no-js h-100" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-      
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>TeamTrack</title>
-    <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="{{ asset('template/styles/shards-dashboards.1.1.0.min.css')  }}">
-    <link rel="stylesheet" href="{{ asset('template/styles/extras.1.1.0.min.css') }}">
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    @include('layouts.appheader')
   </head>
   <body class="h-100">
+
+    <!-- Settings icon -->
     <div class="color-switcher-toggle animated pulse infinite">
       <i class="material-icons">settings</i>
     </div>
+
     <div class="container-fluid">
       <div class="row">
+
         <!-- Main Sidebar -->
         @include('inc.sidebar')
-        <!-- End Main Sidebar -->
+
         <main class="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
           <div class="main-navbar sticky-top bg-white">
             <!-- Main Navbar -->
             @include('inc.navbar')
           </div>
-          <!-- / .main-navbar -->
-          <div class="main-content-container container-fluid px-4">
-            <!-- Page Content -->
-                <!-- Page Header 
-                <div class="page-header row no-gutters py-4">
-                <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
-                    <span class="text-uppercase page-subtitle">Dashboard</span>
-                    <h3 class="page-title">Team Overview</h3>
-                </div>
-                </div>
-                -->
 
+          <div class="main-content-container container-fluid px-4">
+
+              <!-- Content -->
                 <main class="py-4">
                     @yield('content')
                 </main>
-
-            <!-- Page content end -->
            
           </div>
-          <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
 
+          <!-- Footer -->
+          <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
             <span class="copyright ml-auto my-auto mr-2">Copyright © 2019
               <a href="https://designrevision.com" rel="nofollow">TeamTrack</a>
             </span>
           </footer>
+
         </main>
       </div>
     </div>
@@ -66,5 +51,6 @@
     <script src="{{ asset('template/scripts/extras.1.1.0.min.js') }}"></script>
     <script src="{{ asset('template/scripts/shards-dashboards.1.1.0.min.js') }}"></script>
     <script src="{{ asset('template/scripts/app/app-blog-overview.1.1.0.js') }}"></script>
+
   </body>
 </html>
