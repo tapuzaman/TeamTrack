6c1c5f7b265d59dfcdb2c47705642bf34d8971e5
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-07 16:15:20 +0600

    Add Member to team functions added

--- TeamTrack/app/Team.php
+++ TeamTrack/app/Team.php
@@ -16,4 +16,14 @@ class Team extends Model
         return $this->belongsToMany('App\Member','team_user','team_id','user_id');
     }
 
+    public static function addMember($member_id, $team_id){
+        $member = Member::find($member_id);
+        $member->teams()->attach($team_id);
+    }
+
+    public static function addMemberByEmail($member_email, $team_id){
+        $member = Member::where('email',$member_email)->first();
+        $member->teams()->attach($team_id);
+    }
+
 }
