0279a9e256a48cc92c727e9cd872ac8f3453ca08
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-10 17:23:14 +0600

    new team modal

--- TeamTrack/resources/views/about.blade.php
+++ TeamTrack/resources/views/about.blade.php
@@ -11,13 +11,50 @@
     Launch demo modal
     </button>
 
+<!-- Add Team Modal -->
+<div class="modal fade" id="newTaskModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    {!! Form::open(['action' => 'TasksController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Add Task</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        {{Form::label('sprintId', 'Sprint Id')}}
+                        {{Form::text('sprintId', 9, ['class' => 'form-control', 'placeholder' => 'Sprint Id'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('assignedTo', 'Assign Task to')}}
+                        {{Form::select('assignedTo', [], 12, ['class' => 'form-control', "placeholder" => "Pick member"])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('title', 'Task Name')}}
+                        {{Form::text('title', 'Default Title', ['class' => 'form-control', 'placeholder' => 'Task Name'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('description', 'Task Description')}}
+                        {{Form::textarea('description', 'Default Descrption', ['class' => 'form-control', 'placeholder' => 'Task Description'])}}
+                    </div>
+                <div class="modal-footer">
+                    {{Form::submit('Add Task', ['class'=>'btn btn-primary'])}}
+                </div>
+                
+            </div>
+        </div>
+    {!! Form::close() !!}
+</div>
 
     <!-- Modal 1-->
-    <div class="modal fade" id="newTaskModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalCenterTitle">newTaskModal title</h5>
+            <h5 class="modal-title" id="exampleModalCenterTitle">Add Task</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
---
+++ TeamTrack/resources/views/modals/new_task_modal.blade.php
@@ -0,0 +1,37 @@
+<!-- New Task Modal -->
+<div class="modal fade" id="newTaskModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    {!! Form::open(['action' => 'TasksController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Add Task</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="form-group">
+                        {{Form::label('sprintId', 'Sprint Id')}}
+                        {{Form::text('sprintId', 9, ['class' => 'form-control', 'placeholder' => 'Sprint Id'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('assignedTo', 'Assign Task to')}}
+                        {{Form::select('assignedTo', [], 12, ['class' => 'form-control', "placeholder" => "Pick member"])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('title', 'Task Name')}}
+                        {{Form::text('title', 'Default Title', ['class' => 'form-control', 'placeholder' => 'Task Name'])}}
+                    </div>
+                    <div class="form-group">
+                        {{Form::label('description', 'Task Description')}}
+                        {{Form::textarea('description', 'Default Descrption', ['class' => 'form-control', 'placeholder' => 'Task Description'])}}
+                    </div>
+                <div class="modal-footer">
+                    {{Form::submit('Add Task', ['class'=>'btn btn-primary'])}}
+                </div>
+                
+            </div>
+        </div>
+    {!! Form::close() !!}
+</div>
