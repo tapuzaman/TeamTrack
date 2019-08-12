05ed4ae27b9e20e62d1139d20d547375604f8888
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-12 08:41:34 +0600

    Bug removed

--- TeamTrack/resources/views/modals/edit_task_modal.blade.php
+++ TeamTrack/resources/views/modals/edit_task_modal.blade.php
@@ -1,6 +1,6 @@
 <!-- Edit Task Modal -->
 <div class="modal fade" id="editTaskModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    {!! Form::open(['action' =>  ['TasksController@update', $task->id], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
+    {!! Form::open(['action' =>  ['TasksController@update', ''], 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
