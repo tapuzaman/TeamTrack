0d057874bca018315ff6f7c008a1b3949735a6e1
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-16 22:59:06 +0600

    Made policy TeamPolicy

---
+++ TeamTrack/app/Policies/TeamPolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Team;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TeamPolicy
+{
+    use HandlesAuthorization;
+    
+    /**
+     * Determine whether the user can view any teams.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the team.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Team  $team
+     * @return mixed
+     */
+    public function view(User $user, Team $team)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create teams.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the team.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Team  $team
+     * @return mixed
+     */
+    public function update(User $user, Team $team)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the team.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Team  $team
+     * @return mixed
+     */
+    public function delete(User $user, Team $team)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the team.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Team  $team
+     * @return mixed
+     */
+    public function restore(User $user, Team $team)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the team.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Team  $team
+     * @return mixed
+     */
+    public function forceDelete(User $user, Team $team)
+    {
+        //
+    }
+}
