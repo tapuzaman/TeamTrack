707b7ea82cd0a4bd032d02aa3bbf43c1490ae2f3
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-17 03:05:32 +0600

    Policy changed

--- TeamTrack/app/Policies/TeamPolicy.php
+++ TeamTrack/app/Policies/TeamPolicy.php
@@ -10,17 +10,9 @@ class TeamPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view the team.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Team  $team
-     * @return mixed
-     */
     public function viewTeam(User $user, Team $team)
     {
         return $team->members->contains('id',$user->id);
-        //return 1;
     }
 
 }
