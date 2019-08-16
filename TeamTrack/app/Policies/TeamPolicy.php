5b6cd961033ab64664ffe4480d69c2e0b0ea782b
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-17 02:40:30 +0600

    Chnage team policy

--- TeamTrack/app/Policies/TeamPolicy.php
+++ TeamTrack/app/Policies/TeamPolicy.php
@@ -9,17 +9,6 @@ use Illuminate\Auth\Access\HandlesAuthorization;
 class TeamPolicy
 {
     use HandlesAuthorization;
-    
-    /**
-     * Determine whether the user can view any teams.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-       // return 0;
-    }
 
     /**
      * Determine whether the user can view the team.
@@ -28,68 +17,10 @@ class TeamPolicy
      * @param  \App\Team  $team
      * @return mixed
      */
-    public function view(User $user, Team $team)
+    public function viewTeam(User $user, Team $team)
     {
        // return $team->members->contains('id',$user->id);
         return 1;
     }
 
-    /**
-     * Determine whether the user can create teams.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can update the team.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Team  $team
-     * @return mixed
-     */
-    public function update(User $user, Team $team)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can delete the team.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Team  $team
-     * @return mixed
-     */
-    public function delete(User $user, Team $team)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the team.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Team  $team
-     * @return mixed
-     */
-    public function restore(User $user, Team $team)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the team.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Team  $team
-     * @return mixed
-     */
-    public function forceDelete(User $user, Team $team)
-    {
-        //
-    }
 }
