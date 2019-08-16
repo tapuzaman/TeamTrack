73e1f2af9db4f2c63aad23d2c7365d35c7e154bc
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-16 19:29:22 +0600

    Chnage add member view

--- TeamTrack/resources/views/modals/new_member_modal.blade.php
+++ TeamTrack/resources/views/modals/new_member_modal.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Add Task</h5>
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Add Member by email</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
@@ -14,7 +14,7 @@
                     <div class="form-group">
                         {{Form::text('email', '', ['class' => 'form-control', 'placeholder' => 'Email address'])}}
                     </div>
-                   
+                </div>
                 <div class="modal-footer">
                 <a class="new-member-submit">
                     {{Form::submit('Add Member', ['class'=>'btn btn-primary new-task-submit', 'data-dismiss'=>'modal'])}}
