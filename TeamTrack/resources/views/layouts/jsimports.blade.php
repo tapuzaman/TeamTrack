64317a691b804b3d7c6b6ff86d2ce7dd98abb1a0
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-17 15:06:57 +0600

    Toggle check uncheck

--- TeamTrack/resources/views/layouts/jsimports.blade.php
+++ TeamTrack/resources/views/layouts/jsimports.blade.php
@@ -128,16 +128,24 @@
                     console.log('toggleIsCompleted called');
 
                     taskId = $(this).attr('taskId');
+                    isCompleted = e.target.parentElement.querySelector('#taskIsCompleted').innerHTML;
                     sprintId = e.target.parentElement.querySelector('#taskSprintId').innerHTML;
                     assignedTo = e.target.parentElement.querySelector('#taskAssignedToId').innerHTML;
                     title = e.target.parentElement.querySelector('#taskTitle').innerHTML;
                     description = e.target.parentElement.querySelector('#taskDescription').innerHTML;
                     console.log('toggleIsCompleted called. task: '.concat(taskId));
 
+                    //Toggle isCompleted state
+                    if(isCompleted==0){
+                         isCompleted=1;
+                    }else{
+                         isCompleted=0;
+                    }
+
                     $.ajax({
                     type:'PUT',
                     url:'/tasks/'.concat(taskId),
-                    data:{sprintId:sprintId, assignedTo:assignedTo, title:title, description:description},
+                    data:{sprintId:sprintId, isCompleted:isCompleted, assignedTo:assignedTo, title:title, description:description},
                     success:function(data){
                          $('.sprint-view').load( window.location.pathname.concat(' .sprint-view'),
                               function(responseText, textStatus, XMLHttpRequest){
@@ -147,6 +155,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
                          console.log(data.message);
                     } 
@@ -180,6 +189,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
                          console.log(data.message);
                     } 
@@ -221,6 +231,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
                          console.log(data.message);  
                     } 
@@ -249,6 +260,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
                          console.log(data.message);  
                     } 
@@ -280,6 +292,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
 
                     } 
@@ -333,6 +346,7 @@
                                    deleteSprint();
                                    setCommentTaskModalInfo();
                                    newComment();
+                                   toggleIsCompleted()
                          });
                     } 
                     });
