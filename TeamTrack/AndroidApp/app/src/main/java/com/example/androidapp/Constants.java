293e6ec410ba031533edd6f41a5a1ac03c1c1ecd
smashfu <smashfu@gmail.com>
2019-08-18 03:01:05 +0600

    Android studio constants for database connection

---
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/androidapp/Constants.java
@@ -0,0 +1,7 @@
+package com.example.androidapp;
+
+public class Constants {
+    private static final String ROOT_URL= "http://192.168.0.134/AndroidConnection/v1/";
+
+    public static final String URL_REGISTER= ROOT_URL+"registerUser.php";
+}
