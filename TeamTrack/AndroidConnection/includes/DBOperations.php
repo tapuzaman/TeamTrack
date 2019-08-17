5214357b5fba991ad5be1f1e7389ccb74c4e64e8
smashfu <smashfu@gmail.com>
2019-08-18 02:12:10 +0600

    Bycrypt hashing for password

--- TeamTrack/AndroidConnection/includes/DBOperations.php
+++ TeamTrack/AndroidConnection/includes/DBOperations.php
@@ -19,6 +19,7 @@
                 return 0; 
             }else{
                 $password = md5($pass);
+                #$password =password_hash($pass, PASSWORD_DEFAULT);
                 $stmt = $this->con->prepare("INSERT INTO `users`(`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL,?,?,NULL,?,NULL,NULL,NULL)");
                 $stmt->bind_param("sss", $name, $email, $password);
  
