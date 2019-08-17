eb8acdb77501377775dc2bfc2f32d94853fa4cda
smashfu <smashfu@gmail.com>
2019-08-17 22:45:13 +0600

    Database operations registration issues fixed

--- TeamTrack/AndroidConnection/includes/DBOperations.php
+++ TeamTrack/AndroidConnection/includes/DBOperations.php
@@ -14,13 +14,13 @@
         /*CRUD -> C -> CREATE */
         
         # Creates a New User
-        public function createUser($name, $pass, $email){
+        public function createUser($name, $email,$pass){
             if($this->isUserExist($name,$email)){
                 return 0; 
             }else{
                 $password = md5($pass);
                 $stmt = $this->con->prepare("INSERT INTO `users`(`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL,?,?,NULL,?,NULL,NULL,NULL)");
-                $stmt->bind_param("sss", $name, $password, $email);
+                $stmt->bind_param("sss", $name, $email, $password);
  
                 if($stmt->execute()){
                     return 1; 
