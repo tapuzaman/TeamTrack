b48dc3f354f115a0aab4326516f0dd5aba74aeac
smashfu <smashfu@gmail.com>
2019-08-17 16:16:57 +0600

    Database operations insertion updated

--- TeamTrack/AndroidConnection/includes/DBOperations.php
+++ TeamTrack/AndroidConnection/includes/DBOperations.php
@@ -19,8 +19,7 @@
                 return 0; 
             }else{
                 $password = md5($pass);
-                $stmt = $this->con->prepare("INSERT INTO `users` (`id`, `name`, `password`,`email`, `email_verified_at`, `remember_token`, `created_at`, `updated_at`)
-                                                        VALUES (NULL, ?, ?, ?,NULL, NULL, NULL, NULL);");
+                $stmt = $this->con->prepare("INSERT INTO `users`(`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL,?,?,NULL,?,NULL,NULL,NULL)");
                 $stmt->bind_param("sss", $name, $password, $email);
  
                 if($stmt->execute()){
