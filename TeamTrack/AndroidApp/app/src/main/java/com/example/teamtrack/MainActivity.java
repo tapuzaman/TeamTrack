6714453784d9fcd375e04a186ba1d61998da0422
smashfu <smashfu@gmail.com>
2019-08-21 04:24:49 +0600

    main method updated

--- TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/MainActivity.java
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/MainActivity.java
@@ -2,13 +2,87 @@ package com.example.teamtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextName,editTextEmail,editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        progressDialog = new ProgressDialog(this);
+
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+
+
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.setMessage("Registering User");
+        progressDialog.show();
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
+            new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params =new HashMap<>();
+                params.put("name",name);
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+
+
     }
 }
