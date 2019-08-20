0a04914dbf06add0e4e3dde6e8d5835bd4a55537
smashfu <smashfu@gmail.com>
2019-08-21 03:27:39 +0600

    files deleted

--- TeamTrack/AndroidApp/app/src/main/java/com/example/androidapp/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.example.androidapp;
-
-public class Constants {
-    private static final String ROOT_URL= "http://192.168.0.134/AndroidConnection/v1/";
-
-    public static final String URL_REGISTER= ROOT_URL+"registerUser.php";
-}
