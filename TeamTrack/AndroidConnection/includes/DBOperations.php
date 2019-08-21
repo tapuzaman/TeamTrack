965f649b8f23e5b9932c90240b6acacb0110ccae
smashfu <smashfu@gmail.com>
2019-08-22 05:22:45 +0600

    md5 hashing

--- TeamTrack/AndroidConnection/includes/DBOperations.php
+++ TeamTrack/AndroidConnection/includes/DBOperations.php
@@ -18,8 +18,8 @@
             if($this->isUserExist($name,$email)){
                 return 0; 
             }else{
-                #$password = md5($pass);
-                $password =password_hash($pass, PASSWORD_DEFAULT);
+                $password = md5($pass);
+                #$password =password_hash($pass, PASSWORD_DEFAULT);
                 $stmt = $this->con->prepare("INSERT INTO `users`(`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES (NULL,?,?,NULL,?,NULL,NULL,NULL)");
                 $stmt->bind_param("sss", $name, $email, $password);
  
@@ -34,6 +34,7 @@
         # Handles User Login
         public function userLogin($email, $pass){
             $password = md5($pass);
+            #$password =password_hash($pass, PASSWORD_DEFAULT);
             $stmt = $this->con->prepare("SELECT id FROM users WHERE email = ? AND password = ?");
             $stmt->bind_param("ss",$email,$password);
             $stmt->execute();
