fad29c26e091c7e84ebfdee2f82f10a19df6b774
smashfu <smashfu@gmail.com>
2019-08-22 18:44:15 +0600

    userlogin root url added to constants

--- TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/Constants.java
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     private static final String ROOT_URL = "http://192.168.0.134/AndroidConnection/v1/";
 
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
-    //public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
 
 
 }
