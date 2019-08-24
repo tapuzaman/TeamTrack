cc5c6670e73809ad3f700a2130eb1b728f2bb513
smashfu <smashfu@gmail.com>
2019-08-24 21:30:55 +0600

    Login activity

--- TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/LoginActivity.java
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/LoginActivity.java
@@ -2,13 +2,118 @@ package com.example.teamtrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editTextEmail,editTextPassword;
+    private Button buttonLogin;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+
+        buttonLogin.setOnClickListener(this);
+
+    }
+
+    private void userLogin(){
+        final String email = editTextEmail.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        progressDialog.show();
+
+        StringRequest stringRequest= new StringRequest(
+            Request.Method.POST,
+            Constants.URL_LOGIN,
+            new Response.Listener<String>(){
+                @Override
+                public void onResponse(String response) {
+                    progressDialog.dismiss();
+                    try {
+                        JSONObject obj= new JSONObject(response);
+                        if(!obj.getBoolean("error")){
+                            SharedPrefManager.getInstance(getApplicationContext())
+                                .userLogin(
+                                    obj.getInt("id"),
+                                    obj.getString("name"),
+                                    obj.getString("email")
+                                );
+                            Toast.makeText(
+                                getApplicationContext(),
+                                "User login Successful",
+                                Toast.LENGTH_LONG
+                            ).show();
+                        }else {
+
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    obj.getString("message"),
+                                    Toast.LENGTH_LONG
+                            ).show();
+
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            },
+            new Response.ErrorListener(){
+                @Override
+                public  void onErrorResponse(VolleyError error){
+                    progressDialog.dismiss();
+
+                    Toast.makeText(
+                        getApplicationContext(),
+                        error.getMessage(),
+                        Toast.LENGTH_LONG
+                    ).show();
+                }
+            }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String>params= new HashMap<>();
+                params.put("email",email);
+                params.put("password",password);
+                return params;
+            }
+        };
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view==buttonLogin){
+            userLogin();
+        }
+
     }
 }
