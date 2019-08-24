a4711f78b2e72d200c02349095582c9a905c02f4
tapuzaman <tapu.zaman101@gmail.com>
2019-08-25 01:53:31 +0600

    comment edited

--- TeamTrack/config/app.php
+++ TeamTrack/config/app.php
@@ -228,6 +228,8 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        //socialite aliases added
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
 
