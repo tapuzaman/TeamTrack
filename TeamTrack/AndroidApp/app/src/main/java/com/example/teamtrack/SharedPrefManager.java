81af8081a8242955af313a89d5f3e3ce0dc4d273
smashfu <smashfu@gmail.com>
2019-08-25 00:29:21 +0600

    Android Files

---
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/SharedPrefManager.java
@@ -0,0 +1,63 @@
+package com.example.teamtrack;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class SharedPrefManager {
+    private static SharedPrefManager mInstance;
+    private static Context mCtx;
+
+    private static final String SHARED_PREF_NAME = "mysharedpref12";
+    private static final String KEY_USERNAME = "name";
+    private static final String KEY_USER_EMAIL = "useremail";
+    private static final String KEY_USER_ID = "userid";
+
+    private SharedPrefManager(Context context) {
+        mCtx = context;
+
+
+    }
+
+    public static synchronized SharedPrefManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new SharedPrefManager(context);
+        }
+        return mInstance;
+    }
+
+    public boolean userLogin(int id, String name, String email){
+
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt(KEY_USER_ID, id);
+        editor.putString(KEY_USER_EMAIL, email);
+        editor.putString(KEY_USERNAME, name);
+
+        editor.apply();
+
+        return true;
+    }
+
+    public boolean isLoggedIn(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        if(sharedPreferences.getString(KEY_USERNAME, null) != null){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean logout(){
+        SharedPreferences sharedPreferences = mCtx.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
+        return true;
+    }
+
+
+}
