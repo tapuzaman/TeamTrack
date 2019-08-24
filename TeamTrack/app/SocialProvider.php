e0cf4a4a576bb2cde9fa35d3d1505a5a2a49f91f
tapuzaman <tapu.zaman101@gmail.com>
2019-08-25 02:06:28 +0600

    SocialProvider model edited

--- TeamTrack/app/SocialProvider.php
+++ TeamTrack/app/SocialProvider.php
@@ -9,6 +9,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class SocialProvider extends Model
 {
+    //making a column of these in the database table
     protected $fillable = ['provider_id','provider'];
     function user(){
         return $this->belongsTo(User::class);
