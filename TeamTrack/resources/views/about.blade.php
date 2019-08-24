3d29a82784915c8e3d5188419ad639a9cb620920
Mir-Tanvir-Islam <tanvir.islam04@northsouth.edu>
2019-08-24 22:01:29 +0600

    Contact page change

--- TeamTrack/resources/views/about.blade.php
+++ TeamTrack/resources/views/about.blade.php
@@ -7,6 +7,7 @@
         <div class="row justify-content-center card p-4">
             <div class="col-md-8 p-4">
                 <h1>About TeamTrack</h1>
+                <hr>
                 <h5>
                     Make work life simpler, more pleasant and more productive.
                 </h5>
--- TeamTrack/resources/views/contact.blade.php
+++ TeamTrack/resources/views/contact.blade.php
@@ -1,14 +1,17 @@
-@extends('layouts.app')
+<html>
 
-@section('content')
+@include('layouts.cssimports')
 
-    <div id="container">
-
-        <h1>Contact page</h1>
-        <hr>
-        <h5>Email : teamtrack@gmail.com</h5>
-        <h5>Phone : ++880837234</h5>
-
+<body>
+    <div class="container p-4">
+        <div class="row justify-content-center card p-4">
+            <div class="col-md-8 p-4">
+            <h1>Contact Teamtrack</h1>
+            <hr>
+            <h5>Email : teamtrack@gmail.com</h5>
+            <h5>Phone : ++880837234
+            </h5>
+            </div>
+        </div>
     </div>
-
-@endsection
+</body>
