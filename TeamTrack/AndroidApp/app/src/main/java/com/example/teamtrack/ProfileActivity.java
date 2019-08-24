7b63514f01b253870c8130b7b00e58e909cae644
smashfu <smashfu@gmail.com>
2019-08-24 23:06:51 +0600

    logout method added

--- TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/ProfileActivity.java
+++ TeamTrack/AndroidApp/app/src/main/java/com/example/teamtrack/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.teamtrack;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        if(!SharedPrefManager.getInstance(this).isLoggedIn()){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menuLogout:
+                SharedPrefManager.getInstance(this).logout();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+        return true;
     }
 }
